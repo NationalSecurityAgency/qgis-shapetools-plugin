70dbc517dc6c6e75e1d93928967da805f4896b52
C Hamilton <publisher@solarviews.com>
2017-03-09 15:12:42 -0500

    Added a settings dialog box.

---
+++ settings.py
@@ -0,0 +1,46 @@
+import os
+
+from PyQt4 import QtGui, uic
+from PyQt4.QtCore import *
+from qgis.core import *
+from qgis.gui import *
+
+
+FORM_CLASS, _ = uic.loadUiType(os.path.join(
+    os.path.dirname(__file__), 'settings.ui'))
+
+
+class SettingsWidget(QtGui.QDialog, FORM_CLASS):
+    '''Settings Dialog box.'''
+    def __init__(self, iface, parent):
+        super(SettingsWidget, self).__init__(parent)
+        self.setupUi(self)
+        self.iface = iface
+        self.readSettings()
+
+    def readSettings(self):
+        '''Load the user selected settings. The settings are retained even when
+        the user quits QGIS.'''
+        settings = QSettings()
+        self.guessNames = int(settings.value('/ShapeTools/GuessNames', 2))
+        self.maxSegLength =  float(settings.value('/ShapeTools/MaxSegLength', 1000.0))
+        self.maxSegments =  int(settings.value('/ShapeTools/MaxSegments', 400))
+        
+    def accept(self):
+        '''Accept the settings and save them for next time.'''
+        settings = QSettings()
+        settings.setValue('/ShapeTools/GuessNames', self.guessCheckBox.checkState())
+        settings.setValue('/ShapeTools/MaxSegments', self.maxSegmentsSpinBox.value())
+        settings.setValue('/ShapeTools/MaxSegLength', self.segLengthSpinBox.value())
+        self.readSettings()
+        self.close()
+        
+    def showEvent(self, e):
+        '''The user has selected the settings dialog box so we need to
+        read the settings and update the dialog box with the previously
+        selected settings.'''
+        self.readSettings()
+        self.guessCheckBox.setCheckState(self.guessNames)
+        self.maxSegmentsSpinBox.setValue(self.maxSegments)
+        self.segLengthSpinBox.setValue(self.maxSegLength)
+        
