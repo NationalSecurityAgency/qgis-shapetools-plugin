2f9dd3ba12de4d94bcfa610a571186f0a639fcf9
C Hamilton <publisher@solarviews.com>
2017-03-21 14:03:21 -0400

    Added geographiclib

---
+++ ext-libs/geographiclib/__init__.py
@@ -0,0 +1,7 @@
+"""geographiclib: geodesic routines from GeographicLib"""
+
+__version_info__ = (1, 47, 0)
+"""GeographicLib version as a tuple"""
+
+__version__ = "1.47"
+"""GeographicLib version as a string"""
