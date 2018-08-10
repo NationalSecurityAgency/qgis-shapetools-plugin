9e34dcd68572ec4748ab7c0e7e8f390482bda337
C <adenaculture@gmail.com>
2018-08-10 17:25:58 -0400

    new polygon processing algorithm

---
+++ createPolygon.py
@@ -0,0 +1,236 @@
+import os
+import math
+from geographiclib.geodesic import Geodesic
+
+from qgis.core import (QgsVectorLayer,
+    QgsPointXY, QgsFeature, QgsGeometry, 
+    QgsProject, QgsWkbTypes, QgsCoordinateTransform)
+    
+from qgis.core import (QgsProcessing,
+    QgsFeatureSink,
+    QgsProcessingAlgorithm,
+    QgsProcessingParameterNumber,
+    QgsProcessingParameterEnum,
+    QgsProcessingParameterFeatureSource,
+    QgsProcessingParameterField,
+    QgsProcessingParameterFeatureSink)
+
+from qgis.PyQt.QtGui import QIcon
+from qgis.PyQt.QtCore import QUrl
+
+from .settings import epsg4326
+from .utils import *
+    
+geod = Geodesic.WGS84
+
+SHAPE_TYPE=[tr("Polygon"),tr("Line")]
+
+class CreatePolygonAlgorithm(QgsProcessingAlgorithm):
+    """
+    Algorithm to create a donut shape.
+    """
+
+    PrmInputLayer = 'InputLayer'
+    PrmOutputLayer = 'OutputLayer'
+    PrmShapeType = 'ShapeType'
+    PrmNumberOfSidesField = 'NumberOfSidesField'
+    PrmStartingAngleField = 'StartingAngleField'
+    PrmRadiusField = 'RadiusField'
+    PrmDefaultNumberOfSides = 'DefaultNumberOfSides'
+    PrmDefaultStartingAngle = 'DefaultStartingAngle'
+    PrmDefaultRadius = 'DefaultRadius'
+    PrmUnitsOfMeasure = 'UnitsOfMeasure'
+
+    def initAlgorithm(self, config):
+        self.addParameter(
+            QgsProcessingParameterFeatureSource(
+                self.PrmInputLayer,
+                tr('Input point layer'),
+                [QgsProcessing.TypeVectorPoint])
+        )
+        self.addParameter(
+            QgsProcessingParameterEnum(
+                self.PrmShapeType,
+                tr('Shape type'),
+                options=SHAPE_TYPE,
+                defaultValue=0,
+                optional=False)
+        )
+        self.addParameter(
+            QgsProcessingParameterField(
+                self.PrmNumberOfSidesField,
+                tr('Number of sides field'),
+                parentLayerParameterName=self.PrmInputLayer,
+                type=QgsProcessingParameterField.Any,
+                optional=True
+            )
+        )
+        self.addParameter(
+            QgsProcessingParameterField(
+                self.PrmStartingAngleField,
+                tr('Starting angle field'),
+                parentLayerParameterName=self.PrmInputLayer,
+                type=QgsProcessingParameterField.Any,
+                optional=True
+            )
+        )
+        self.addParameter(
+            QgsProcessingParameterField(
+                self.PrmRadiusField,
+                tr('Radius field'),
+                parentLayerParameterName=self.PrmInputLayer,
+                type=QgsProcessingParameterField.Any,
+                optional=True
+            )
+        )
+        self.addParameter(
+            QgsProcessingParameterNumber(
+                self.PrmDefaultNumberOfSides,
+                tr('Default number of sides'),
+                QgsProcessingParameterNumber.Integer,
+                defaultValue=3,
+                minValue=3,
+                optional=True)
+            )
+        self.addParameter(
+            QgsProcessingParameterNumber(
+                self.PrmDefaultStartingAngle,
+                tr('Default starting angle'),
+                QgsProcessingParameterNumber.Double,
+                defaultValue=0,
+                optional=True)
+            )
+        self.addParameter(
+            QgsProcessingParameterNumber(
+                self.PrmDefaultRadius,
+                tr('Default radius'),
+                QgsProcessingParameterNumber.Double,
+                defaultValue=40.0,
+                minValue=0,
+                optional=True)
+            )
+        self.addParameter(
+            QgsProcessingParameterEnum(
+                self.PrmUnitsOfMeasure,
+                tr('Radius units'),
+                options=DISTANCE_LABELS,
+                defaultValue=0,
+                optional=False)
+        )
+        self.addParameter(
+            QgsProcessingParameterFeatureSink(
+                self.PrmOutputLayer,
+                tr('Output layer'))
+            )
+    
+    def processAlgorithm(self, parameters, context, feedback):
+        source = self.parameterAsSource(parameters, self.PrmInputLayer, context)
+        shapetype = self.parameterAsInt(parameters, self.PrmShapeType, context)
+        sidescol = self.parameterAsString(parameters, self.PrmNumberOfSidesField, context)
+        anglecol = self.parameterAsString(parameters, self.PrmStartingAngleField, context)
+        distcol = self.parameterAsString(parameters, self.PrmRadiusField, context)
+        sides = self.parameterAsInt(parameters, self.PrmDefaultNumberOfSides, context)
+        angle = self.parameterAsDouble(parameters, self.PrmDefaultStartingAngle, context)
+        defaultDist = self.parameterAsInt(parameters, self.PrmDefaultRadius, context)
+        unitOfDist = self.parameterAsInt(parameters, self.PrmUnitsOfMeasure, context)
+        
+        measureFactor = conversionToMeters(unitOfDist)
+            
+        defaultDist *= measureFactor
+        
+        srcCRS = source.sourceCrs()
+        if shapetype == 0:
+            (sink, dest_id) = self.parameterAsSink(parameters,
+                self.PrmOutputLayer, context, source.fields(),
+                QgsWkbTypes.Polygon, srcCRS)
+        else:
+            (sink, dest_id) = self.parameterAsSink(parameters,
+                self.PrmOutputLayer, context, source.fields(),
+                QgsWkbTypes.LineString, srcCRS)
+                
+        if srcCRS != epsg4326:
+            geomTo4326 = QgsCoordinateTransform(srcCRS, epsg4326, QgsProject.instance())
+            toSinkCrs = QgsCoordinateTransform(epsg4326, srcCRS, QgsProject.instance())
+        
+        featureCount = source.featureCount()
+        total = 100.0 / featureCount if featureCount else 0
+        
+        iterator = source.getFeatures()
+        for cnt, feature in enumerate(iterator):
+            if feedback.isCanceled():
+                break
+            try:
+                pt = feature.geometry().asPoint()
+                if srcCRS != epsg4326:
+                    pt = geomTo4326.transform(pt.x(), pt.y())
+                if sidescol:
+                    s = int(feature[sidescol])
+                else:
+                    s = sides
+                if anglecol:
+                    startangle = float(feature[anglecol])
+                else:
+                    startangle = angle
+                if distcol:
+                    d = float(feature[distcol])*measureFactor
+                else:
+                    d = defaultDist
+                pts = []
+                i = s
+                while i >= 0:
+                    a = (i * 360.0 / s)+startangle
+                    i -= 1
+                    g = geod.Direct(pt.y(), pt.x(), a, d, Geodesic.LATITUDE | Geodesic.LONGITUDE)
+                    pts.append(QgsPointXY(g['lon2'], g['lat2']))
+                    
+                # If the Output crs is not 4326 transform the points to the proper crs
+                if srcCRS != epsg4326:
+                    for x, ptout in enumerate(pts):
+                        pts[x] = toSinkCrs.transform(ptout)
+
+                f = QgsFeature()
+                if shapetype == 0:
+                    f.setGeometry(QgsGeometry.fromPolygonXY([pts]))
+                else:
+                    f.setGeometry(QgsGeometry.fromPolylineXY(pts))
+                f.setAttributes(feature.attributes())
+                sink.addFeature(f)
+            except:
+                pass
+                
+            feedback.setProgress(int(cnt * total))
+            
+        return {self.PrmOutputLayer: dest_id}
+        
+    def name(self):
+        return 'createpolygon'
+
+    def icon(self):
+        return QIcon(os.path.join(os.path.dirname(__file__),'images/polygon.png'))
+    
+    def displayName(self):
+        return tr('Create polygon')
+    
+    def group(self):
+        return tr('Geodesic vector creation')
+        
+    def groupId(self):
+        return 'vectorcreation'
+        
+    def helpUrl(self):
+        file = os.path.dirname(__file__)+'/index.html'
+        if not os.path.exists(file):
+            return ''
+        return QUrl.fromLocalFile(file).toString(QUrl.FullyEncoded)
+    
+    '''def shortHelpString(self):
+        file = os.path.dirname(__file__)+'/doc/GeodesicDensifyAlgorithm.help'
+        if not os.path.exists(file):
+            return ''
+        with open(file) as helpf:
+            help=helpf.read()
+        return help'''
+        
+    def createInstance(self):
+        return CreatePolygonAlgorithm()
+
