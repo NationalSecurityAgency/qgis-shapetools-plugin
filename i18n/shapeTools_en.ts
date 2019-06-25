29f088ed3e958387e0d8b56aa014ae50a55a084d
C <adenaculture@gmail.com>
2019-06-25 10:41:22 -0400

    Update shapeTools_en.ts

--- i18n/shapeTools_en.ts
+++ i18n/shapeTools_en.ts
@@ -63,27 +63,27 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../shapeTools.py" line="79"/>
+        <location filename="../shapeTools.py" line="95"/>
         <source>Create shapes</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../shapeTools.py" line="92"/>
+        <location filename="../shapeTools.py" line="108"/>
         <source>XY to Line</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../shapeTools.py" line="100"/>
+        <location filename="../shapeTools.py" line="116"/>
         <source>Geodesic shape densifier</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../idlbreakline.py" line="110"/>
+        <location filename="../idlbreakline.py" line="105"/>
         <source>Geodesic line break at -180,180</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../shapeTools.py" line="117"/>
+        <location filename="../shapeTools.py" line="133"/>
         <source>Geodesic measure tool</source>
         <translation type="unfinished"></translation>
     </message>
@@ -93,22 +93,22 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../geodesicTransformation.py" line="160"/>
+        <location filename="../geodesicTransformation.py" line="156"/>
         <source>Geodesic transformations</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../geodesicFlip.py" line="114"/>
+        <location filename="../geodesicFlip.py" line="111"/>
         <source>Geodesic flip and rotate</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../shapeTools.py" line="143"/>
+        <location filename="../shapeTools.py" line="159"/>
         <source>Flip horizontal</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../shapeTools.py" line="147"/>
+        <location filename="../shapeTools.py" line="163"/>
         <source>Flip vertical</source>
         <translation type="unfinished"></translation>
     </message>
@@ -128,162 +128,162 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../shapeTools.py" line="162"/>
+        <location filename="../shapeTools.py" line="178"/>
         <source>Geodesic Transforms</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../shapeTools.py" line="175"/>
+        <location filename="../shapeTools.py" line="191"/>
         <source>Azimuth distance digitizer</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../shapeTools.py" line="185"/>
+        <location filename="../shapeTools.py" line="201"/>
         <source>Azimuth distance sequence digitizer</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../shapeTools.py" line="195"/>
+        <location filename="../shapeTools.py" line="211"/>
         <source>Settings</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../shapeTools.py" line="202"/>
+        <location filename="../shapeTools.py" line="218"/>
         <source>Shape Tools help</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../idlbreakline.py" line="30"/>
+        <location filename="../idlbreakline.py" line="27"/>
         <source>Input layer</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../xyToLine.py" line="56"/>
+        <location filename="../xyToLine.py" line="54"/>
         <source>Input CRS for coordinates within the vector fields</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../xyToLine.py" line="62"/>
+        <location filename="../xyToLine.py" line="60"/>
         <source>Output layer CRS</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../xyToLine.py" line="68"/>
+        <location filename="../xyToLine.py" line="66"/>
         <source>Line type</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../xyToLine.py" line="76"/>
+        <location filename="../xyToLine.py" line="74"/>
         <source>Use the point geometry for the line starting point</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../xyToLine.py" line="83"/>
+        <location filename="../xyToLine.py" line="81"/>
         <source>Starting X Field (lon)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../xyToLine.py" line="92"/>
+        <location filename="../xyToLine.py" line="90"/>
         <source>Starting Y Field (lat)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../xyToLine.py" line="101"/>
+        <location filename="../xyToLine.py" line="99"/>
         <source>Use the point geometry for the line ending point</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../xyToLine.py" line="108"/>
+        <location filename="../xyToLine.py" line="106"/>
         <source>Ending X Field (lon)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../xyToLine.py" line="117"/>
+        <location filename="../xyToLine.py" line="115"/>
         <source>Ending Y Field (lat)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../xyToLine.py" line="126"/>
+        <location filename="../xyToLine.py" line="124"/>
         <source>Show starting point</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../xyToLine.py" line="133"/>
+        <location filename="../xyToLine.py" line="131"/>
         <source>Show ending point</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../xyToLine.py" line="140"/>
+        <location filename="../xyToLine.py" line="138"/>
         <source>Break lines at -180, 180 boundary for better rendering</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../xyToLine.py" line="147"/>
+        <location filename="../xyToLine.py" line="145"/>
         <source>Output line layer</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../xyToLine.py" line="152"/>
+        <location filename="../xyToLine.py" line="150"/>
         <source>Output point layer</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../xyToLine.py" line="194"/>
+        <location filename="../xyToLine.py" line="192"/>
         <source>Output point layer was set to [skip output]. No point layer will be generated.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../xyToLine.py" line="198"/>
+        <location filename="../xyToLine.py" line="196"/>
         <source>In order to use the layer geometry for the start or ending points, the input layer must be of type Point</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../xyToLine.py" line="203"/>
+        <location filename="../xyToLine.py" line="201"/>
         <source>Please select valid starting and ending point columns</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../xyToLine.py" line="207"/>
+        <location filename="../xyToLine.py" line="205"/>
         <source>In order to select the input layer&apos;s geometry as a beginning or ending point it must be a Point vector layer.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../xyToLine.py" line="314"/>
+        <location filename="../xyToLine.py" line="312"/>
         <source>{} out of {} features from the input layer were invalid and were ignored.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../xyToLine.py" line="326"/>
+        <location filename="../xyToLine.py" line="323"/>
         <source>XY to line</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../geodesicTransformation.py" line="163"/>
+        <location filename="../geodesicTransformation.py" line="159"/>
         <source>Vector geometry</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../settings.py" line="151"/>
+        <location filename="../settings.py" line="152"/>
         <source>Azimuth Range -180 to 180</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../settings.py" line="151"/>
+        <location filename="../settings.py" line="152"/>
         <source>Azimuth Range 0 tp 360</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../settings.py" line="152"/>
+        <location filename="../settings.py" line="153"/>
         <source>System Ellipsoids</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../settings.py" line="152"/>
+        <location filename="../settings.py" line="153"/>
         <source>Historical Ellipsoids</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../geodesicLayerMeasure.py" line="39"/>
+        <location filename="../geodesicLayerMeasure.py" line="40"/>
         <source>Line or polygon layer</source>
         <translation type="unfinished"></translation>
     </message>
@@ -313,7 +313,7 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../geodesicLayerMeasure.py" line="45"/>
+        <location filename="../geodesicLayerMeasure.py" line="46"/>
         <source>Measure total length rather than each line segment</source>
         <translation type="unfinished"></translation>
     </message>
@@ -323,32 +323,32 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../geodesicLayerMeasure.py" line="60"/>
+        <location filename="../geodesicLayerMeasure.py" line="61"/>
         <source>Use automatic styling</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../geodesicMeasureTool.py" line="100"/>
+        <location filename="../geodesicMeasureTool.py" line="99"/>
         <source>Heading To</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../geodesicMeasureTool.py" line="100"/>
+        <location filename="../geodesicMeasureTool.py" line="99"/>
         <source>Heading From</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../geodesicMeasureTool.py" line="100"/>
+        <location filename="../geodesicMeasureTool.py" line="99"/>
         <source>Distance</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../geodesicMeasureTool.py" line="142"/>
+        <location filename="../geodesicMeasureTool.py" line="141"/>
         <source>Ellipsoid: </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../lineDigitizer.py" line="59"/>
+        <location filename="../lineDigitizer.py" line="58"/>
         <source>Clicked location is invalid</source>
         <translation type="unfinished"></translation>
     </message>
@@ -358,17 +358,17 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../lineDigitizer.py" line="90"/>
+        <location filename="../lineDigitizer.py" line="88"/>
         <source>No point or line layer selected</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../lineDigitizer.py" line="80"/>
+        <location filename="../lineDigitizer.py" line="78"/>
         <source>Enter bearing distance pairs</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../lineDigitizer.py" line="86"/>
+        <location filename="../lineDigitizer.py" line="84"/>
         <source>One or more entered values were invalid</source>
         <translation type="unfinished"></translation>
     </message>
@@ -598,42 +598,42 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../utils.py" line="10"/>
+        <location filename="../utils.py" line="11"/>
         <source>Kilometers</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../utils.py" line="10"/>
+        <location filename="../utils.py" line="11"/>
         <source>Meters</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../utils.py" line="10"/>
+        <location filename="../utils.py" line="11"/>
         <source>Centimeters</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../utils.py" line="10"/>
+        <location filename="../utils.py" line="11"/>
         <source>Miles</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../utils.py" line="10"/>
+        <location filename="../utils.py" line="11"/>
         <source>Yards</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../utils.py" line="10"/>
+        <location filename="../utils.py" line="11"/>
         <source>Feet</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../utils.py" line="10"/>
+        <location filename="../utils.py" line="11"/>
         <source>Inches</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../utils.py" line="10"/>
+        <location filename="../utils.py" line="11"/>
         <source>Nautical Miles</source>
         <translation type="unfinished"></translation>
     </message>
