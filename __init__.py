3c8854f2889441fc48a0fa3a635d459d4a6116ac
C Hamilton <hamiltoncj@users.noreply.github.com>
2020-03-16 16:34:42 -0400

    if geographiclib is not available use local copy

--- __init__.py
+++ __init__.py
@@ -1,7 +1,11 @@
-import os
-import site
-
-site.addsitedir(os.path.abspath(os.path.dirname(__file__) + '/ext-libs'))
+from qgis.core import Qgis
+# geographiclib was added to QGIS around QGIS 3.12
+try:
+    import geographiclib
+except Exception:
+    import os
+    import site
+    site.addsitedir(os.path.abspath(os.path.dirname(__file__) + '/ext-libs'))
 
 def classFactory(iface):
     from .shapeTools import ShapeTools
