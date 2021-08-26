d7d443b5092873e09159eb65bcbb277305d25d64
C Hamilton <hamiltoncj@users.noreply.github.com>
2021-08-26 15:59:37 -0400

    Algorithm enhanced to use data defined overrides.

--- createLob.py
+++ createLob.py
@@ -3,18 +3,16 @@ import math
 from geographiclib.geodesic import Geodesic
 
 from qgis.core import (
-    QgsPointXY, QgsFeature, QgsGeometry, QgsField,
-    QgsProject, QgsWkbTypes, QgsCoordinateTransform)
+    QgsPointXY, QgsGeometry, QgsField,
+    QgsProject, QgsWkbTypes, QgsCoordinateTransform, QgsPropertyDefinition)
 
 from qgis.core import (
     QgsProcessing,
-    QgsProcessingAlgorithm,
+    QgsProcessingFeatureBasedAlgorithm,
+    QgsProcessingParameters,
     QgsProcessingParameterBoolean,
     QgsProcessingParameterNumber,
-    QgsProcessingParameterEnum,
-    QgsProcessingParameterFeatureSource,
-    QgsProcessingParameterField,
-    QgsProcessingParameterFeatureSink)
+    QgsProcessingParameterEnum)
 
 from qgis.PyQt.QtGui import QIcon
 from qgis.PyQt.QtCore import QVariant, QUrl
@@ -22,172 +20,19 @@ from qgis.PyQt.QtCore import QVariant, QUrl
 from .settings import epsg4326, geod, settings
 from .utils import tr, conversionToMeters, makeIdlCrossingsPositive, DISTANCE_LABELS
 
-class CreateLobAlgorithm(QgsProcessingAlgorithm):
+
+class CreateLobAlgorithm(QgsProcessingFeatureBasedAlgorithm):
     """
     Algorithm to create a line of bearing.
     """
 
-    PrmInputLayer = 'InputLayer'
-    PrmOutputLayer = 'OutputLayer'
-    PrmAzimuthField = 'AzimuthField'
-    PrmDistanceField = 'DistanceField'
-    PrmDefaultAzimuth = 'DefaultAzimuth'
-    PrmDefaultDistance = 'DefaultDistance'
-    PrmUnitsOfMeasure = 'UnitsOfMeasure'
+    PrmAzimuth = 'Azimuth'
+    PrmDistance = 'Distance'
+    PrmUnits = 'Units'
     PrmExportInputGeometry = 'ExportInputGeometry'
 
-    def initAlgorithm(self, config):
-        self.addParameter(
-            QgsProcessingParameterFeatureSource(
-                self.PrmInputLayer,
-                tr('Input point layer'),
-                [QgsProcessing.TypeVectorPoint])
-        )
-        self.addParameter(
-            QgsProcessingParameterField(
-                self.PrmAzimuthField,
-                tr('Azimuth/bearing field'),
-                parentLayerParameterName=self.PrmInputLayer,
-                type=QgsProcessingParameterField.Any,
-                optional=True
-            )
-        )
-        self.addParameter(
-            QgsProcessingParameterField(
-                self.PrmDistanceField,
-                tr('Distance field'),
-                parentLayerParameterName=self.PrmInputLayer,
-                type=QgsProcessingParameterField.Any,
-                optional=True
-            )
-        )
-        self.addParameter(
-            QgsProcessingParameterNumber(
-                self.PrmDefaultAzimuth,
-                tr('Default azimuth/bearing'),
-                QgsProcessingParameterNumber.Double,
-                defaultValue=0,
-                optional=True)
-        )
-        self.addParameter(
-            QgsProcessingParameterNumber(
-                self.PrmDefaultDistance,
-                tr('Default distance'),
-                QgsProcessingParameterNumber.Double,
-                defaultValue=1000.0,
-                minValue=0,
-                optional=True)
-        )
-        self.addParameter(
-            QgsProcessingParameterEnum(
-                self.PrmUnitsOfMeasure,
-                tr('Distance units'),
-                options=DISTANCE_LABELS,
-                defaultValue=0,
-                optional=False)
-        )
-        self.addParameter(
-            QgsProcessingParameterBoolean(
-                self.PrmExportInputGeometry,
-                tr('Add input geometry fields to output table'),
-                False,
-                optional=True)
-        )
-        self.addParameter(
-            QgsProcessingParameterFeatureSink(
-                self.PrmOutputLayer,
-                tr('Output layer'))
-        )
-
-    def processAlgorithm(self, parameters, context, feedback):
-        source = self.parameterAsSource(parameters, self.PrmInputLayer, context)
-        bearingcol = self.parameterAsString(parameters, self.PrmAzimuthField, context)
-        distcol = self.parameterAsString(parameters, self.PrmDistanceField, context)
-        defaultBearing = self.parameterAsDouble(parameters, self.PrmDefaultAzimuth, context)
-        defaultDist = self.parameterAsDouble(parameters, self.PrmDefaultDistance, context)
-        units = self.parameterAsInt(parameters, self.PrmUnitsOfMeasure, context)
-        export_geom = self.parameterAsBool(parameters, self.PrmExportInputGeometry, context)
-
-        measureFactor = conversionToMeters(units)
-
-        defaultDist *= measureFactor
-        maxseglen = settings.maxSegLength * 1000.0  # Needs to be in meters
-        maxSegments = settings.maxSegments
-
-        srcCRS = source.sourceCrs()
-        fields = source.fields()
-        if export_geom:
-            names = fields.names()
-            name_x, name_y = settings.getGeomNames(names)
-            fields.append(QgsField(name_x, QVariant.Double))
-            fields.append(QgsField(name_y, QVariant.Double))
-        (sink, dest_id) = self.parameterAsSink(
-            parameters, self.PrmOutputLayer, context, fields,
-            QgsWkbTypes.LineString, srcCRS)
-
-        if srcCRS != epsg4326:
-            geomTo4326 = QgsCoordinateTransform(srcCRS, epsg4326, QgsProject.instance())
-            toSinkCrs = QgsCoordinateTransform(epsg4326, srcCRS, QgsProject.instance())
-
-        featureCount = source.featureCount()
-        total = 100.0 / featureCount if featureCount else 0
-
-        num_features = 0
-        numbad = 0
-        iterator = source.getFeatures()
-        for cnt, feature in enumerate(iterator):
-            if feedback.isCanceled():
-                break
-            num_features += 1
-            try:
-                if bearingcol:
-                    bearing = float(feature[bearingcol])
-                else:
-                    bearing = defaultBearing
-                if distcol:
-                    distance = float(feature[distcol]) * measureFactor
-                else:
-                    distance = defaultDist
-                pt = feature.geometry().asPoint()
-                pt_orig_x = pt.x()
-                pt_orig_y = pt.y()
-                # make sure the coordinates are in EPSG:4326
-                if srcCRS != epsg4326:
-                    pt = geomTo4326.transform(pt.x(), pt.y())
-                pts = [pt]
-                gline = geod.Line(pt.y(), pt.x(), bearing)
-                n = int(math.ceil(distance / maxseglen))
-                if n > maxSegments:
-                    n = maxSegments
-                seglen = distance / n
-                for i in range(1, n + 1):
-                    s = seglen * i
-                    g = gline.Position(s, Geodesic.LATITUDE | Geodesic.LONGITUDE | Geodesic.LONG_UNROLL)
-                    pts.append(QgsPointXY(g['lon2'], g['lat2']))
-
-                makeIdlCrossingsPositive(pts)
-                # If the Output crs is not 4326 transform the points to the proper crs
-                if srcCRS != epsg4326:
-                    for x, ptout in enumerate(pts):
-                        pts[x] = toSinkCrs.transform(ptout)
-
-                f = QgsFeature()
-                f.setGeometry(QgsGeometry.fromPolylineXY(pts))
-                attr = feature.attributes()
-                if export_geom:
-                    attr.append(pt_orig_x)
-                    attr.append(pt_orig_y)
-                f.setAttributes(attr)
-                sink.addFeature(f)
-            except Exception:
-                numbad += 1
-
-            feedback.setProgress(int(cnt * total))
-
-        if numbad > 0:
-            feedback.pushInfo(tr("{} out of {} features had invalid parameters and were ignored.".format(numbad, featureCount)))
-
-        return {self.PrmOutputLayer: dest_id}
+    def createInstance(self):
+        return CreateLobAlgorithm()
 
     def name(self):
         return 'createlob'
@@ -204,11 +49,143 @@ class CreateLobAlgorithm(QgsProcessingAlgorithm):
     def groupId(self):
         return 'vectorcreation'
 
+    def outputName(self):
+        return tr('Output layer')
+
     def helpUrl(self):
         file = os.path.dirname(__file__) + '/index.html'
         if not os.path.exists(file):
             return ''
         return QUrl.fromLocalFile(file).toString(QUrl.FullyEncoded)
 
-    def createInstance(self):
-        return CreateLobAlgorithm()
+    def inputLayerTypes(self):
+        return [QgsProcessing.TypeVectorPoint]
+
+    def outputWkbType(self, input_wkb_type):
+        return (QgsWkbTypes.LineString)
+
+    def outputFields(self, input_fields):
+        if self.export_geom:
+            name_x, name_y = settings.getGeomNames(input_fields.names())
+            input_fields.append(QgsField(name_x, QVariant.Double))
+            input_fields.append(QgsField(name_y, QVariant.Double))
+        return(input_fields)
+
+    def initParameters(self, config=None):
+        param = QgsProcessingParameterNumber(
+            self.PrmAzimuth,
+            tr('Azimuth/bearing'),
+            QgsProcessingParameterNumber.Double,
+            defaultValue=0,
+            optional=True)
+        param.setIsDynamic(True)
+        param.setDynamicPropertyDefinition(QgsPropertyDefinition(
+            self.PrmAzimuth,
+            tr('Azimuth/bearing'),
+            QgsPropertyDefinition.Double))
+        param.setDynamicLayerParameterName('INPUT')
+        self.addParameter(param)
+
+        param = QgsProcessingParameterNumber(
+            self.PrmDistance,
+            tr('Distance'),
+            QgsProcessingParameterNumber.Double,
+            defaultValue=1000.0,
+            minValue=0,
+            optional=True)
+        param.setIsDynamic(True)
+        param.setDynamicPropertyDefinition(QgsPropertyDefinition(
+            self.PrmDistance,
+            tr('Distance'),
+            QgsPropertyDefinition.Double))
+        param.setDynamicLayerParameterName('INPUT')
+        self.addParameter(param)
+
+        self.addParameter(
+            QgsProcessingParameterEnum(
+                self.PrmUnits,
+                tr('Distance units'),
+                options=DISTANCE_LABELS,
+                defaultValue=0,
+                optional=False)
+        )
+        self.addParameter(
+            QgsProcessingParameterBoolean(
+                self.PrmExportInputGeometry,
+                tr('Add input geometry fields to output table'),
+                False,
+                optional=True)
+        )
+
+    def prepareAlgorithm(self, parameters, context, feedback):
+        self.azimuth = self.parameterAsDouble(parameters, self.PrmAzimuth, context)
+        self.azimuth_dyn = QgsProcessingParameters.isDynamic(parameters, self.PrmAzimuth)
+        if self.azimuth_dyn:
+            self.azimuth_property = parameters[self.PrmAzimuth]
+        self.dist = self.parameterAsDouble(parameters, self.PrmDistance, context)
+        self.dist_dyn = QgsProcessingParameters.isDynamic(parameters, self.PrmDistance)
+        if self.dist_dyn:
+            self.dist_property = parameters[self.PrmDistance]
+        units = self.parameterAsInt(parameters, self.PrmUnits, context)
+        self.export_geom = self.parameterAsBool(parameters, self.PrmExportInputGeometry, context)
+
+        self.measureFactor = conversionToMeters(units)
+
+        self.dist_converted = self.dist * self.measureFactor
+        self.maxseglen = settings.maxSegLength * 1000.0  # Needs to be in meters
+        self.maxSegments = settings.maxSegments
+
+        source = self.parameterAsSource(parameters, 'INPUT', context)
+        srcCRS = source.sourceCrs()
+
+        if srcCRS != epsg4326:
+            self.geomTo4326 = QgsCoordinateTransform(srcCRS, epsg4326, QgsProject.instance())
+            self.toSinkCrs = QgsCoordinateTransform(epsg4326, srcCRS, QgsProject.instance())
+        else:
+            self.geomTo4326 = None
+            self.toSinkCrs = None
+        return True
+
+    def processFeature(self, feature, context, feedback):
+        try:
+            if self.azimuth_dyn:
+                bearing = self.azimuth_property.valueAsDouble(context.expressionContext(), self.azimuth)[0]
+            else:
+                bearing = self.azimuth
+            if self.dist_dyn:
+                distance = self.dist_property.valueAsDouble(context.expressionContext(), self.dist)[0] * self.measureFactor
+            else:
+                distance = self.dist_converted
+            pt = feature.geometry().asPoint()
+            pt_orig_x = pt.x()
+            pt_orig_y = pt.y()
+            # make sure the coordinates are in EPSG:4326
+            if self.geomTo4326:
+                pt = self.geomTo4326.transform(pt.x(), pt.y())
+            pts = [pt]
+            gline = geod.Line(pt.y(), pt.x(), bearing)
+            n = int(math.ceil(distance / self.maxseglen))
+            if n > self.maxSegments:
+                n = self.maxSegments
+            seglen = distance / n
+            for i in range(1, n + 1):
+                s = seglen * i
+                g = gline.Position(s, Geodesic.LATITUDE | Geodesic.LONGITUDE | Geodesic.LONG_UNROLL)
+                pts.append(QgsPointXY(g['lon2'], g['lat2']))
+
+            makeIdlCrossingsPositive(pts)
+            # If the Output crs is not 4326 transform the points to the proper crs
+            if self.toSinkCrs:
+                for x, ptout in enumerate(pts):
+                    pts[x] = self.toSinkCrs.transform(ptout)
+
+            feature.setGeometry(QgsGeometry.fromPolylineXY(pts))
+            if self.export_geom:
+                attr = feature.attributes()
+                attr.append(pt_orig_x)
+                attr.append(pt_orig_y)
+                feature.setAttributes(attr)
+        except Exception:
+            return []
+
+        return [feature]
