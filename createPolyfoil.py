4a84c1cb69d1574a1dfc9d0b88985f21b1dc85e7
C Hamilton <hamiltoncj@users.noreply.github.com>
2021-09-01 17:06:44 +0000

    Algorithm enhanced to use data defined overrides.

--- createPolyfoil.py
+++ createPolyfoil.py
@@ -3,18 +3,16 @@ import math
 from geographiclib.geodesic import Geodesic
 
 from qgis.core import (
-    QgsPointXY, QgsFeature, QgsGeometry, QgsField,
+    QgsPointXY, QgsGeometry, QgsField, QgsPropertyDefinition,
     QgsProject, QgsWkbTypes, QgsCoordinateTransform)
 
 from qgis.core import (
     QgsProcessing,
-    QgsProcessingAlgorithm,
+    QgsProcessingParameters,
+    QgsProcessingFeatureBasedAlgorithm,
     QgsProcessingParameterBoolean,
     QgsProcessingParameterNumber,
-    QgsProcessingParameterEnum,
-    QgsProcessingParameterFeatureSource,
-    QgsProcessingParameterField,
-    QgsProcessingParameterFeatureSink)
+    QgsProcessingParameterEnum)
 
 from qgis.PyQt.QtGui import QIcon
 from qgis.PyQt.QtCore import QVariant, QUrl
@@ -24,17 +22,13 @@ from .utils import tr, conversionToMeters, makeIdlCrossingsPositive, DISTANCE_LA
 
 SHAPE_TYPE = [tr("Polygon"), tr("Line")]
 
-class CreatePolyfoilAlgorithm(QgsProcessingAlgorithm):
+
+class CreatePolyfoilAlgorithm(QgsProcessingFeatureBasedAlgorithm):
     """CreatePolyfoilAlgorithm
     Algorithm to create a hypocycloid shape.
     """
 
-    PrmInputLayer = 'InputLayer'
-    PrmOutputLayer = 'OutputLayer'
     PrmShapeType = 'ShapeType'
-    PrmLobesField = 'LobesField'
-    PrmStartingAngleField = 'StartingAngleField'
-    PrmRadiusField = 'RadiusField'
     PrmLobes = 'Lobes'
     PrmRadius = 'Radius'
     PrmStartingAngle = 'StartingAngle'
@@ -42,13 +36,49 @@ class CreatePolyfoilAlgorithm(QgsProcessingAlgorithm):
     PrmDrawingSegments = 'DrawingSegments'
     PrmExportInputGeometry = 'ExportInputGeometry'
 
-    def initAlgorithm(self, config):
-        self.addParameter(
-            QgsProcessingParameterFeatureSource(
-                self.PrmInputLayer,
-                tr('Input point layer'),
-                [QgsProcessing.TypeVectorPoint])
-        )
+    def createInstance(self):
+        return CreatePolyfoilAlgorithm()
+
+    def name(self):
+        return 'createpolyfoil'
+
+    def icon(self):
+        return QIcon(os.path.join(os.path.dirname(__file__), 'images/polyfoil.png'))
+
+    def displayName(self):
+        return tr('Create polyfoil')
+
+    def group(self):
+        return tr('Geodesic vector creation')
+
+    def groupId(self):
+        return 'vectorcreation'
+
+    def outputName(self):
+        return tr('Output layer')
+
+    def helpUrl(self):
+        file = os.path.dirname(__file__) + '/index.html'
+        if not os.path.exists(file):
+            return ''
+        return QUrl.fromLocalFile(file).toString(QUrl.FullyEncoded)
+
+    def inputLayerTypes(self):
+        return [QgsProcessing.TypeVectorPoint]
+
+    def outputWkbType(self, input_wkb_type):
+        if self.shape_type == 0:
+            return (QgsWkbTypes.Polygon)
+        return (QgsWkbTypes.LineString)
+
+    def outputFields(self, input_fields):
+        if self.export_geom:
+            name_x, name_y = settings.getGeomNames(input_fields.names())
+            input_fields.append(QgsField(name_x, QVariant.Double))
+            input_fields.append(QgsField(name_y, QVariant.Double))
+        return(input_fields)
+
+    def initParameters(self, config=None):
         self.addParameter(
             QgsProcessingParameterEnum(
                 self.PrmShapeType,
@@ -57,59 +87,50 @@ class CreatePolyfoilAlgorithm(QgsProcessingAlgorithm):
                 defaultValue=0,
                 optional=False)
         )
-        self.addParameter(
-            QgsProcessingParameterField(
-                self.PrmLobesField,
-                tr('Number of lobes field'),
-                parentLayerParameterName=self.PrmInputLayer,
-                type=QgsProcessingParameterField.Any,
-                optional=True
-            )
-        )
-        self.addParameter(
-            QgsProcessingParameterField(
-                self.PrmStartingAngleField,
-                tr('Starting angle field'),
-                parentLayerParameterName=self.PrmInputLayer,
-                type=QgsProcessingParameterField.Any,
-                optional=True
-            )
-        )
-        self.addParameter(
-            QgsProcessingParameterField(
-                self.PrmRadiusField,
-                tr('Radius field'),
-                parentLayerParameterName=self.PrmInputLayer,
-                type=QgsProcessingParameterField.Any,
-                optional=True
-            )
-        )
-        self.addParameter(
-            QgsProcessingParameterNumber(
-                self.PrmLobes,
-                tr('Number of lobes'),
-                QgsProcessingParameterNumber.Integer,
-                defaultValue=5,
-                minValue=3,
-                optional=True)
-        )
-        self.addParameter(
-            QgsProcessingParameterNumber(
-                self.PrmStartingAngle,
-                tr('Starting angle'),
-                QgsProcessingParameterNumber.Double,
-                defaultValue=0,
-                optional=True)
-        )
-        self.addParameter(
-            QgsProcessingParameterNumber(
-                self.PrmRadius,
-                tr('Radius'),
-                QgsProcessingParameterNumber.Double,
-                defaultValue=40.0,
-                minValue=0,
-                optional=True)
-        )
+        param = QgsProcessingParameterNumber(
+            self.PrmLobes,
+            tr('Number of lobes'),
+            QgsProcessingParameterNumber.Integer,
+            defaultValue=5,
+            minValue=3,
+            optional=False)
+        param.setIsDynamic(True)
+        param.setDynamicPropertyDefinition(QgsPropertyDefinition(
+            self.PrmLobes,
+            tr('Number of lobes'),
+            QgsPropertyDefinition.Integer))
+        param.setDynamicLayerParameterName('INPUT')
+        self.addParameter(param)
+
+        param = QgsProcessingParameterNumber(
+            self.PrmStartingAngle,
+            tr('Starting angle'),
+            QgsProcessingParameterNumber.Double,
+            defaultValue=0,
+            optional=False)
+        param.setIsDynamic(True)
+        param.setDynamicPropertyDefinition(QgsPropertyDefinition(
+            self.PrmStartingAngle,
+            tr('Starting angle'),
+            QgsPropertyDefinition.Double))
+        param.setDynamicLayerParameterName('INPUT')
+        self.addParameter(param)
+
+        param = QgsProcessingParameterNumber(
+            self.PrmRadius,
+            tr('Radius'),
+            QgsProcessingParameterNumber.Double,
+            defaultValue=40.0,
+            minValue=0,
+            optional=False)
+        param.setIsDynamic(True)
+        param.setDynamicPropertyDefinition(QgsPropertyDefinition(
+            self.PrmRadius,
+            tr('Radius'),
+            QgsPropertyDefinition.Double))
+        param.setDynamicLayerParameterName('INPUT')
+        self.addParameter(param)
+
         self.addParameter(
             QgsProcessingParameterEnum(
                 self.PrmUnitsOfMeasure,
@@ -134,82 +155,78 @@ class CreatePolyfoilAlgorithm(QgsProcessingAlgorithm):
                 False,
                 optional=True)
         )
-        self.addParameter(
-            QgsProcessingParameterFeatureSink(
-                self.PrmOutputLayer,
-                tr('Output layer'))
-        )
 
-    def processAlgorithm(self, parameters, context, feedback):
-        source = self.parameterAsSource(parameters, self.PrmInputLayer, context)
-        shapetype = self.parameterAsInt(parameters, self.PrmShapeType, context)
-        lobescol = self.parameterAsString(parameters, self.PrmLobesField, context)
-        startanglecol = self.parameterAsString(parameters, self.PrmStartingAngleField, context)
-        radiuscol = self.parameterAsString(parameters, self.PrmRadiusField, context)
-        radius = self.parameterAsDouble(parameters, self.PrmRadius, context)
-        startAngle = self.parameterAsDouble(parameters, self.PrmStartingAngle, context)
-        lobes = self.parameterAsInt(parameters, self.PrmLobes, context)
+    def prepareAlgorithm(self, parameters, context, feedback):
+        self.shape_type = self.parameterAsInt(parameters, self.PrmShapeType, context)
+        self.radius = self.parameterAsDouble(parameters, self.PrmRadius, context)
+        if self.radius <= 0:
+            feedback.reportError('Radius parameter must be greater than 0')
+            return False
+        self.radius_dyn = QgsProcessingParameters.isDynamic(parameters, self.PrmRadius)
+        if self.radius_dyn:
+            self.radius_property = parameters[self.PrmRadius]
+        self.start_angle = self.parameterAsDouble(parameters, self.PrmStartingAngle, context)
+        self.start_angle_dyn = QgsProcessingParameters.isDynamic(parameters, self.PrmStartingAngle)
+        if self.start_angle_dyn:
+            self.start_angle_property = parameters[self.PrmStartingAngle]
+        self.lobes = self.parameterAsInt(parameters, self.PrmLobes, context)
+        self.lobes_dyn = QgsProcessingParameters.isDynamic(parameters, self.PrmLobes)
+        if self.lobes_dyn:
+            self.lobes_property = parameters[self.PrmLobes]
         segments = self.parameterAsInt(parameters, self.PrmDrawingSegments, context)
         units = self.parameterAsInt(parameters, self.PrmUnitsOfMeasure, context)
-        export_geom = self.parameterAsBool(parameters, self.PrmExportInputGeometry, context)
+        self.export_geom = self.parameterAsBool(parameters, self.PrmExportInputGeometry, context)
 
-        measureFactor = conversionToMeters(units)
-        radius *= measureFactor
-        r = radius / lobes
+        self.measureFactor = conversionToMeters(units)
+        self.radius_converted = self.radius * self.measureFactor
 
+        self.step = 360.0 / segments
+
+        source = self.parameterAsSource(parameters, 'INPUT', context)
         srcCRS = source.sourceCrs()
-        fields = source.fields()
-        if export_geom:
-            names = fields.names()
-            name_x, name_y = settings.getGeomNames(names)
-            fields.append(QgsField(name_x, QVariant.Double))
-            fields.append(QgsField(name_y, QVariant.Double))
-        if shapetype == 0:
-            (sink, dest_id) = self.parameterAsSink(
-                parameters, self.PrmOutputLayer, context, fields,
-                QgsWkbTypes.Polygon, srcCRS)
-        else:
-            (sink, dest_id) = self.parameterAsSink(
-                parameters, self.PrmOutputLayer, context, fields,
-                QgsWkbTypes.LineString, srcCRS)
+        self.total_features = source.featureCount()
 
         if srcCRS != epsg4326:
-            geomTo4326 = QgsCoordinateTransform(srcCRS, epsg4326, QgsProject.instance())
-            toSinkCrs = QgsCoordinateTransform(epsg4326, srcCRS, QgsProject.instance())
+            self.geomTo4326 = QgsCoordinateTransform(srcCRS, epsg4326, QgsProject.instance())
+            self.toSinkCrs = QgsCoordinateTransform(epsg4326, srcCRS, QgsProject.instance())
+        else:
+            self.geomTo4326 = None
+            self.toSinkCrs = None
+        self.num_bad = 0
+        return True
 
-        featureCount = source.featureCount()
-        total = 100.0 / featureCount if featureCount else 0
-
-        step = 360.0 / segments
-        numbad = 0
-        iterator = source.getFeatures()
-        for index, feature in enumerate(iterator):
-            if feedback.isCanceled():
-                break
-            try:
-                if startanglecol:
-                    sangle = float(feature[startanglecol])
-                else:
-                    sangle = startAngle
-                if lobescol:
-                    lobes2 = int(feature[lobescol])
-                else:
-                    lobes2 = lobes
-                if radiuscol:
-                    radius2 = float(feature[radiuscol]) * measureFactor
-                else:
-                    radius2 = radius
-                r = radius2 / lobes2
-            except Exception:
-                numbad += 1
-                continue
+    def processFeature(self, feature, context, feedback):
+        try:
+            if self.start_angle_dyn:
+                sangle, e = self.start_angle_property.valueAsDouble(context.expressionContext(), self.start_angle)
+                if not e:
+                    self.num_bad += 1
+                    return []
+            else:
+                sangle = self.start_angle
+            if self.lobes_dyn:
+                lobes2, e = self.lobes_property.valueAsInt(context.expressionContext(), self.lobes)
+                if not e or lobes2 < 3:
+                    self.num_bad += 1
+                    return []
+            else:
+                lobes2 = self.lobes
+            if self.radius_dyn:
+                radius2, e = self.radius_property.valueAsDouble(context.expressionContext(), self.radius)
+                if not e or radius2 <= 0:
+                    self.num_bad += 1
+                    return []
+                radius2 *= self.measureFactor
+            else:
+                radius2 = self.radius_converted
+            r = radius2 / lobes2
             pts = []
             pt = feature.geometry().asPoint()
             pt_orig_x = pt.x()
             pt_orig_y = pt.y()
             # make sure the coordinates are in EPSG:4326
-            if srcCRS != epsg4326:
-                pt = geomTo4326.transform(pt.x(), pt.y())
+            if self.geomTo4326:
+                pt = self.geomTo4326.transform(pt.x(), pt.y())
             angle = 0.0
             while angle <= 360.0:
                 a = math.radians(angle - sangle)
@@ -218,51 +235,29 @@ class CreatePolyfoilAlgorithm(QgsProcessingAlgorithm):
                 dist = math.sqrt(x * x + y * y)
                 g = geod.Direct(pt.y(), pt.x(), angle, dist, Geodesic.LATITUDE | Geodesic.LONGITUDE)
                 pts.append(QgsPointXY(g['lon2'], g['lat2']))
-                angle += step
+                angle += self.step
 
             makeIdlCrossingsPositive(pts)
             # If the Output crs is not 4326 transform the points to the proper crs
-            if srcCRS != epsg4326:
+            if self.toSinkCrs:
                 for x, ptout in enumerate(pts):
-                    pts[x] = toSinkCrs.transform(ptout)
+                    pts[x] = self.toSinkCrs.transform(ptout)
 
-            f = QgsFeature()
-            if shapetype == 0:
-                f.setGeometry(QgsGeometry.fromPolygonXY([pts]))
+            if self.shape_type == 0:
+                feature.setGeometry(QgsGeometry.fromPolygonXY([pts]))
             else:
-                f.setGeometry(QgsGeometry.fromPolylineXY(pts))
-            attr = feature.attributes()
-            if export_geom:
+                feature.setGeometry(QgsGeometry.fromPolylineXY(pts))
+            if self.export_geom:
+                attr = feature.attributes()
                 attr.append(pt_orig_x)
                 attr.append(pt_orig_y)
-            f.setAttributes(attr)
-            sink.addFeature(f)
+                feature.setAttributes(attr)
+        except Exception:
+            self.num_bad += 1
+            return []
+        return [feature]
 
-            if index % 100 == 0:
-                feedback.setProgress(int(index * total))
-
-        return {self.PrmOutputLayer: dest_id}
-
-    def name(self):
-        return 'createpolyfoil'
-
-    def icon(self):
-        return QIcon(os.path.join(os.path.dirname(__file__), 'images/polyfoil.png'))
-
-    def displayName(self):
-        return tr('Create polyfoil')
-
-    def group(self):
-        return tr('Geodesic vector creation')
-
-    def groupId(self):
-        return 'vectorcreation'
-
-    def helpUrl(self):
-        file = os.path.dirname(__file__) + '/index.html'
-        if not os.path.exists(file):
-            return ''
-        return QUrl.fromLocalFile(file).toString(QUrl.FullyEncoded)
-
-    def createInstance(self):
-        return CreatePolyfoilAlgorithm()
+    def postProcessAlgorithm(self, context, feedback):
+        if self.num_bad:
+            feedback.pushInfo(tr("{} out of {} features had invalid parameters and were ignored.".format(self.num_bad, self.total_features)))
+        return {}
