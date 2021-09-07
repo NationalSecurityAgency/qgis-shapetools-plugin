b68a467891e41ec2cf734d72d03fa1732c9d4db0
C Hamilton <hamiltoncj@users.noreply.github.com>
2021-09-07 19:34:52 +0000

    add st_geodesic_transform() expression

--- stFunctions.py
+++ stFunctions.py
@@ -1,5 +1,6 @@
 import re
-from qgis.core import QgsUnitTypes, QgsPointXY, QgsGeometry, QgsExpression, QgsCoordinateReferenceSystem, QgsCoordinateTransform, QgsProject, QgsWkbTypes
+from geographiclib.geodesic import Geodesic
+from qgis.core import QgsUnitTypes, QgsPointXY, QgsPoint, QgsGeometry, QgsExpression, QgsCoordinateReferenceSystem, QgsCoordinateTransform, QgsProject, QgsWkbTypes
 from qgis.utils import qgsfunction
 from .settings import epsg4326, geod, settings
 
@@ -24,12 +25,14 @@ def InitShapeToolsFunctions():
     QgsExpression.registerFunction(st_to_meters)
     QgsExpression.registerFunction(st_geodesic_distance)
     QgsExpression.registerFunction(st_geodesic_bearing)
+    QgsExpression.registerFunction(st_geodesic_transform)
 
 def UnloadShapeToolsFunctions():
     QgsExpression.unregisterFunction('st_from_meters')
     QgsExpression.unregisterFunction('st_to_meters')
     QgsExpression.unregisterFunction('st_geodesic_distance')
     QgsExpression.unregisterFunction('st_geodesic_bearing')
+    QgsExpression.unregisterFunction('st_geodesic_transform')
 
 @qgsfunction(args=2, group=group_name)
 def st_from_meters(values, feature, parent):
@@ -44,7 +47,7 @@ def st_from_meters(values, feature, parent):
     <li><i>length</i> &rarr; the length in meters to be converted.</li>
     <li><i>units</i> &rarr; conversion unit</li>
     <ul>
-    <li><i>'cm<'/i> &rarr; centimeters</li>
+    <li><i>'cm'</i> &rarr; centimeters</li>
     <li><i>'m'</i> &rarr; meters</li>
     <li><i>'km'</i> &rarr; kilometers</li>
     <li><i>'in'</i> &rarr; inches</li>
@@ -98,7 +101,7 @@ def st_to_meters(values, feature, parent):
     <li><i>length</i> &rarr; the length to be converted.</li>
     <li><i>units</i> &rarr; units of the length</li>
     <ul>
-    <li><i>'cm<'/i> &rarr; centimeters</li>
+    <li><i>'cm'</i> &rarr; centimeters</li>
     <li><i>'m'</i> &rarr; meters</li>
     <li><i>'km'</i> &rarr; kilometers</li>
     <li><i>'in'</i> &rarr; inches</li>
@@ -210,7 +213,7 @@ def st_geodesic_bearing(values, feature, parent):
 
     <h4>Syntax</h4>
     <p><b>st_geodesic_bearing</b>( <i>y1, x1, y2, x2[, crs='EPSG:4326']</i> )</p>
-    <p><b>st_geodesic_bearing</b>( <i>geom_1, geom_2[, crs='EPSG:4326']</i> )</p>
+    <p><b>st_geodesic_bearing</b>( <i>geom1, geom2[, crs='EPSG:4326']</i> )</p>
 
     <h4>Arguments</h4>
     <p><i>y1</i> &rarr; the y or latitude coordinate for the first point.<br />
@@ -263,4 +266,120 @@ def st_geodesic_bearing(values, feature, parent):
         return(l['azi1'])
     except Exception:
         parent.setEvalErrorString("Error: invalid  parameters")
+        return
+
+@qgsfunction(args=-1, group=group_name)
+def st_geodesic_transform(values, feature, parent):
+    """
+    Geodesically transfrom a shape (point, line, polygon) using rotation, translation, and scaling.
+
+    <h4>Syntax</h4>
+    <p><b>st_geodesic_transform</b>( <i>geom [, scale=1, rotate=0, distance=0, azimuth=0, unit='m', crs='EPSG:4326'</i>)</p>
+
+    <h4>Arguments</h4>
+    <ul>
+    <li><i>geom</i> &rarr; input geometry (point, line, polygon).</li>
+    <li><i>scale</i> &rarr; the scale factor. Default is 1.0.</li>
+    <li><i>rotate</i> &rarr; the rotation angle in degrees. Default is 0 degrees.</li>
+    <li><i>distance</i> &rarr; the translation distance. Default is 0.</li>
+    <li><i>azimuth</i> &rarr; the translation azimuth in degrees. Default is 0.</li>
+    <li><i>unit</i> &rarr; translation distance units</li>
+    <ul>
+    <li><i>'cm'</i> &rarr; centimeters</li>
+    <li><i>'m'</i> &rarr; meters</li>
+    <li><i>'km'</i> &rarr; kilometers</li>
+    <li><i>'in'</i> &rarr; inches</li>
+    <li><i>'ft'</i> &rarr; feet</li>
+    <li><i>'yard'</i> &rarr; yards</li>
+    <li><i>'mi'</i> &rarr; miles</li>
+    <li><i>'nm'</i> &rarr; nautical miles</li>
+    </ul></ul>
+
+    <h4>Example usage</h4>
+    <ul>
+      <li>geom_to_wkt(st_geodesic_transform(make_line(make_point(2,4),make_point(3,5)), 1.0, 45, 1000, 90, 'km')) &rarr; returns 'LineString (10.80438811 4.44561039, 12.21595371 4.44309579)'</li>
+    </ul>
+    """
+    num_args = len(values)
+    if num_args < 1 or num_args > 7:
+        parent.setEvalErrorString("Error: invalid number of arguments")
+        return
+    scale = 1.0
+    rotate = 0.0
+    distance = 0.0
+    azimuth = 0.0
+    unit = 'm'
+    crs = 'EPSG:4326'
+    try:
+        geom = values[0]
+        if num_args > 1:
+            scale = float(values[1])
+        if num_args > 2:
+            rotate = float(values[2])
+        if num_args > 3:
+            distance = float(values[3])
+        if num_args > 4:
+            azimuth = float(values[4])
+        if num_args > 5:
+            unit = values[5]
+        if num_args > 6:
+            crs = values[6]
+            
+        crs = QgsCoordinateReferenceSystem(crs)
+        geom_to_4326 = QgsCoordinateTransform(crs, epsg4326, QgsProject.instance())
+        to_crs = QgsCoordinateTransform(epsg4326, crs, QgsProject.instance())
+
+
+        if unit == 'cm':
+            factor = 0.01
+        elif unit == 'm':
+            factor = 1.0
+        elif unit == 'km':
+            factor = 1000.0
+        elif unit == 'in':
+            factor = QgsUnitTypes.fromUnitToUnitFactor(QgsUnitTypes.DistanceFeet, QgsUnitTypes.DistanceMeters) / 12.0
+        elif unit == 'ft':
+            factor = QgsUnitTypes.fromUnitToUnitFactor(QgsUnitTypes.DistanceFeet, QgsUnitTypes.DistanceMeters)
+        elif unit == 'yard':
+            factor = QgsUnitTypes.fromUnitToUnitFactor(QgsUnitTypes.DistanceYards, QgsUnitTypes.DistanceMeters)
+        elif unit == 'mi':
+            factor = QgsUnitTypes.fromUnitToUnitFactor(QgsUnitTypes.DistanceMiles, QgsUnitTypes.DistanceMeters)
+        elif unit == 'nm':
+            factor = QgsUnitTypes.fromUnitToUnitFactor(QgsUnitTypes.DistanceNauticalMiles, QgsUnitTypes.DistanceMeters)
+        else:
+            parent.setEvalErrorString("Error: invalid unit")
+
+        centroid = geom.centroid().asPoint()
+        centroid = geom_to_4326.transform(centroid.x(), centroid.y())
+        cy = centroid.y()
+        cx = centroid.x()
+        if distance != 0:
+            distance *= factor
+            g = geod.Direct(cy, cx, azimuth, distance, Geodesic.LATITUDE | Geodesic.LONGITUDE)
+            new_centroid = QgsPoint(g['lon2'], g['lat2'])
+        else:
+            new_centroid = centroid
+
+        # Find the x & y coordinates of the new centroid
+        ncy = new_centroid.y()
+        ncx = new_centroid.x()
+
+        vertices = geom.vertices()
+        for vcnt, vertex in enumerate(vertices):
+            v = geom_to_4326.transform(vertex.x(), vertex.y())
+            gline = geod.Inverse(cy, cx, v.y(), v.x())
+            vdist = gline['s12']
+            vazi = gline['azi1']
+            if scale != 1:
+                vdist = vdist * scale
+            if rotate != 0:
+                vazi += rotate
+            g = geod.Direct(ncy, ncx, vazi, vdist, Geodesic.LATITUDE | Geodesic.LONGITUDE)
+            new_vertex = to_crs.transform(g['lon2'], g['lat2'])
+            geom.moveVertex(new_vertex.x(), new_vertex.y(), vcnt)
+        return(geom)
+    except Exception:
+        s = traceback.format_exc()
+        parent.setEvalErrorString(s)
+        # parent.setEvalErrorString("Error: invalid input")
         return
